4e74e3bc2ca4ed71e730d8d7afaab818a6c2ba69
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-02-02 01:17:33 +0900

    Add Constraint on/off switch classes only MySQL.

--- src/main/java/com/github/kazuhito_m/fw/custom/dbunit/CleanInsertWithoutConstraintOperation.java
+++ src/main/java/com/github/kazuhito_m/fw/custom/dbunit/CleanInsertWithoutConstraintOperation.java
@@ -3,11 +3,15 @@ package com.github.kazuhito_m.fw.custom.dbunit;
 import org.dbunit.operation.CompositeOperation;
 import org.dbunit.operation.DatabaseOperation;
 
+import com.github.kazuhito_m.fw.custom.dbunit.mysql.ConstraintSwitchOperation;
+
 public class CleanInsertWithoutConstraintOperation extends CompositeOperation {
 
-    public static final DatabaseOperation CLEAN_INSERT_WITHOUT_CONSTRAINT = new CleanInsertWithoutConstraintOperation();
-    
-    public CleanInsertWithoutConstraintOperation() {
-        super(new DatabaseOperation[]{CLEAN_INSERT});
-    }
+	public static final DatabaseOperation CLEAN_INSERT_WITHOUT_CONSTRAINT = new CleanInsertWithoutConstraintOperation();
+
+	public CleanInsertWithoutConstraintOperation() {
+		super(new DatabaseOperation[] { new ConstraintSwitchOperation(false),
+				CLEAN_INSERT, new ConstraintSwitchOperation(true) });
+	}
+
 }
